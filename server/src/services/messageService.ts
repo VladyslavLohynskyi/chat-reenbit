025431602ec5179738681b071334168bc51e45cf
VladyslavLohynskyi <watcher.good2003@gmail.com>
2024-08-21 23:57:43 +0300

    added setvice to get all messages from chat

--- server/src/services/messageService.ts
+++ server/src/services/messageService.ts
@@ -5,6 +5,10 @@ class MessageService {
       const message = await MessageModel.create({ userId, chatId, content });
       return message;
    }
+   async getMessagesByChatId(chatId: string) {
+      const messages = await MessageModel.find({ chatId });
+      return messages;
+   }
 }
 
 export default new MessageService();
