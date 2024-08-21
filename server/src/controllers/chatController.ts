c4d12f100f763425e043422d2815ba2b9ff04d93
VladyslavLohynskyi <watcher.good2003@gmail.com>
2024-08-21 20:21:45 +0300

    added ChatService with create chat function, added fuctionality to create chat to ChatController

--- server/src/controllers/chatController.ts
+++ server/src/controllers/chatController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { IUser } from '../models/userModel';
 import UserService from '../services/userService';
+import ChatService from '../services/chatService';
+import ChatModel from '../models/chatModel';
 
 interface IChatControllerCreateRequest extends Request {
    body: {
@@ -11,11 +13,16 @@ interface IChatControllerCreateRequest extends Request {
 class chatController {
    async create(req: IChatControllerCreateRequest, res: Response) {
       const { users } = req.body;
-      const user1: IUser = await UserService.createUser(users[0]);
-      const user2: IUser = await UserService.createUser(users[0]);
+      const user1 = await UserService.createUser(users[0]);
+      const user2 = await UserService.createUser(users[1]);
+      const chat = await ChatService.createChat(
+         user1._id.toString(),
+         user2._id.toString(),
+      );
       return res.json({
          message: 'Users created',
          users: [user1, user2],
+         chat,
       });
    }
 }
