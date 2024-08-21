d10fed796857af734959e0d3b268b65b8c92cdf1
VladyslavLohynskyi <watcher.good2003@gmail.com>
2024-08-21 17:39:10 +0300

    added connection to db in server

--- server/global.d.ts
+++ server/global.d.ts
@@ -1,5 +1,6 @@
 namespace NodeJS {
    interface ProcessEnv {
       PORT: string;
+      DB_URL: string;
    }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,20 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express, { Application, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 const app: Application = express();
 const port: number = Number(process.env.PORT) || 8881;
 
 app.use(express.json());
+app.use(cors());
 app.get('/', (req: Request, res: Response) => {
    res.send('Welcome to Express & TypeScript Server');
 });
 
 const start = async () => {
    try {
+      await mongoose.connect(process.env.DB_URL);
       app.listen(port, () => {
          console.log(`Connected successfully on port ${port}`);
       });
