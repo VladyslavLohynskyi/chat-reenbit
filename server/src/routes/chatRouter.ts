95a4c95c5d96c523f8c8738d19a24009da5096ff
VladyslavLohynskyi <watcher.good2003@gmail.com>
2024-08-24 01:27:07 +0300

    added search chats route

--- server/src/routes/chatRouter.ts
+++ server/src/routes/chatRouter.ts
@@ -5,3 +5,4 @@ export const chatRouter = Router();
 chatRouter.post('/', chatController.create);
 chatRouter.delete('/:chatId', chatController.delete);
 chatRouter.get('/:userId', chatController.getUserChats);
+chatRouter.get('/', chatController.searchChats);
