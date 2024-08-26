24ed46a9ac63f08436960dcb639afccf67289c29
VladyslavLohynskyi <watcher.good2003@gmail.com>
2024-08-27 01:04:57 +0300

    added console.log for env variables

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,9 @@ import cors from 'cors';
 import path from 'path';
 const app: Application = express();
 const port: number = Number(process.env.PORT) || 8881;
-
+console.log(process.env.DB_URL);
+console.log(process.env.MODE);
+console.log(process.env.PORT);
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
@@ -28,6 +30,7 @@ const start = async () => {
       });
    } catch (error) {
       console.log('Connecting Error');
+      console.log(error);
    }
 };
 
