b856173ec77294f5f80d6315d9844fa0216d4456
VladyslavLohynskyi <watcher.good2003@gmail.com>
2024-08-26 15:57:30 +0300

    added production mode

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,21 @@ import express, { Application, Request, Response } from 'express';
 import { router } from './routes/index';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
 const app: Application = express();
 const port: number = Number(process.env.PORT) || 8881;
 
 app.use(express.json());
 app.use(cors());
 app.use('/api', router);
+
+if (process.env.MODE === 'production') {
+   app.use(express.static(path.join(__dirname, '../../client/build')));
+
+   app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+   });
+}
 
 const start = async () => {
    try {
